61e23d4927d193bda7c3cfed3d54d67d9a17970b
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-09-30 00:12:20 -0400

    :recycle: Refactor extra gf contributors discovery
    find_gfs_with_extra_contributors returns a tripartite response,
    separating visible/hidden/mixed. find_descendants_by_kfids specifically
    allows only hiding gfs with _hidden_ external contributors.
    
    Combined these make it straightforward to add
    hide_all_descendants_by_filter and show_all_descendants_by_filter
    functions.

--- kf_utils/dataservice/descendants.py
+++ kf_utils/dataservice/descendants.py
@@ -2,25 +2,33 @@
 Methods for finding descendant entities (participants in families, biospecimens
 in those participants, etc).
 """
+from kf_utils.dataservice.patch import hide_kfids, show_kfids
 from kf_utils.dataservice.scrape import yield_entities, yield_kfids
 
 
-def find_genomic_files_with_extra_contributors(host, bs_kfids, gf_kfids=None):
+def find_gfs_with_extra_contributors(host, bs_kfids, gf_kfids=None):
     """
     Given a set of biospecimen KFIDs, find the KFIDs of descendant genomic
-    files that also descend from biospecimes that aren't included in the given
-    set. If you already know the set of descendant genomic files, you may pass
-    them in to save some time.
-
-    e.g. If BS_1234567 and BS_7654321 both contribute to GF_11112222, but you
-    only specify one of the two BSIDs, then GF_11112222 will be returned.
-    If you specify both of them, then GF_11112222 will _not_ be returned.
+    files that also descend from biospecimens that aren't included in the given
+    set. If you already know the full set of descendant genomic files, you may
+    pass them in to save some time.
 
     :param host: dataservice_api_host
-    ("https://kf-api-dataservice.kidsfirstdrc.org")
+        e.g. "https://kf-api-dataservice.kidsfirstdrc.org"
     :param bs_kfids: iterable of biospecimen KFIDs
     :param gf_kfids: iterable of genomic file KFIDs (optional)
-    :returns: set of genomic file KFIDs satisfying the above definition
+    :returns: sets of KFIDs of genomic files with contributing biospecimens not
+        included in bs_kfids, divided into these groups:
+            "all_visible": all extra contributors are visible in the dataservice
+            "all_hidden": all extra contributors are hidden in the dataservice
+            "mixed_visibility": some extra contributors are hidden and some not
+
+    Example:
+    If BS_12345678 and BS_87654321 both contribute to GF_11112222, but you only
+    specify one of the two BSIDs, then GF_11112222 will be returned. If you
+    specify both of them, then GF_11112222 will _not_ be returned. The exact
+    nature of the return will depend on the visibility of the extra
+    contributors.
     """
     if not gf_kfids:
         gens = set()
@@ -31,24 +39,38 @@ def find_genomic_files_with_extra_contributors(host, bs_kfids, gf_kfids=None):
     else:
         gens = set(gf_kfids)
 
-    has_extra_contributors = set()
+    has_extra_contributors = {
+        "mixed_visibility": set(),
+        "hidden": set(),
+        "visible": set(),
+    }
     for g in gens:
-        contributors = set(yield_kfids(
-            host, "biospecimens", {"genomic_file_id": g}, show_progress=True
-        ))
-        if not contributors.issubset(bs_kfids):
-            has_extra_contributors.add(g)
+        contribs = {
+            e["kf_id"]: (e["visible"] == True)  # just in case any are null?
+            for e in yield_entities(
+                host, "biospecimens", {"genomic_file_id": g}, show_progress=True
+            )
+        }
+        contrib_kfids = set(contribs.keys())
+        bs_kfids = set(bs_kfids)
+        if not contrib_kfids.issubset(bs_kfids):
+            extra_kfids = contrib_kfids - bs_kfids
+            extras_visible = set(contribs[k] for k in extra_kfids)
+            if (False in extras_visible) and (True in extras_visible):
+                has_extra_contributors["mixed_visibility"].add(g)
+            elif False in extras_visible:
+                has_extra_contributors["all_hidden"].add(g)
+            else:
+                has_extra_contributors["all_visible"].add(g)
     return has_extra_contributors
 
 
 def find_descendants_by_kfids(
-    host, start_endpoint, start_kfids, include_gfs_with_external_contributors=False
+    host, start_endpoint, start_kfids, ignore_gfs_with_hidden_external_contribs
 ):
     """
     Given a set of KFIDs from a specified endpoint, find the KFIDs of all
     descendant entities.
-
-    e.g.
 
     Given a family kfid, the result will be all participants in that family,
     all of the participants' biospecimens/outcomes/phenotypes/etc, all of
@@ -58,12 +80,20 @@ def find_descendants_by_kfids(
     Given a set of genomic file kfids, the result will be just their sequencing
     experiments and read groups.
 
+    If you plan to make the discovered descendants visible, you should set
+    ignore_gfs_with_hidden_external_contribs=True so that you don't accidentally
+    unhide a genomic file that has hidden contributing biospecimens.
+
+    If you plan to make the discovered descendants hidden, you should set
+    ignore_gfs_with_hidden_external_contribs=False so that everything linked to
+    the hidden biospecimens also get hidden.
+
     :param host: dataservice_api_host ("https://kf-api-dataservice.kidsfirstdrc.org")
     :param start_endpoint: endpoint of the starting kfids being passed in
     :param start_kfids: starting kfids associated with the start_endpoint
-    :param include_gfs_with_external_contributors: whether to also include
-        genomic files (and descendants) that contain information from biospecimens
-        unrelated to the given start_kfids.
+    :param ignore_gfs_with_hidden_external_contribs: whether to ignore
+        genomic files (and their descendants) that contain information from
+        hidden biospecimens unrelated to the given start_kfids.
     :returns: dict mapping endpoints to their sets of discovered kfids
     """
     # Map of direct foreign key descendancy from families down to genomic files
@@ -94,8 +124,8 @@ def find_descendants_by_kfids(
         host,
         endpoint,
         kfids,
-        include_gfs_with_external_contributors=False,
-        descendant_kfids=None,
+        ignore_gfs_with_hidden_external_contribs,
+        descendant_kfids,
     ):
         for (child_endpoint, foreign_key) in descendancy.get(endpoint, []):
             descendant_kfids[child_endpoint] = set()
@@ -104,21 +134,22 @@ def find_descendants_by_kfids(
                     set(yield_kfids(host, child_endpoint, {foreign_key: k}, show_progress=True))
                 )
             if (
-                child_endpoint == "genomic-files"
-            ) and not include_gfs_with_external_contributors:
-                # ignore multi-specimen genomic files with contributors not in
-                # the specified set
-                descendant_kfids[
-                    "genomic-files"
-                ] -= find_genomic_files_with_extra_contributors(
+                (child_endpoint == "genomic-files")
+                and ignore_gfs_with_hidden_external_contribs
+            ):
+                # Ignore multi-specimen genomic files that have hidden
+                # contributing specimens which are not in the descendants
+                extra_contrib_gfs = find_gfs_with_extra_contributors(
                     host, descendant_kfids["biospecimens"],
                     descendant_kfids["genomic-files"]
                 )
+                descendant_kfids["genomic-files"] -= extra_contrib_gfs["all_hidden"]
+                descendant_kfids["genomic-files"] -= extra_contrib_gfs["mixed_visibility"]
             _inner(
                 host,
                 child_endpoint,
                 descendant_kfids[child_endpoint],
-                include_gfs_with_external_contributors,
+                ignore_gfs_with_hidden_external_contribs,
                 descendant_kfids,
             )
 
@@ -127,14 +158,14 @@ def find_descendants_by_kfids(
         host,
         start_endpoint,
         start_kfids,
-        include_gfs_with_external_contributors,
+        ignore_gfs_with_hidden_external_contribs,
         descendant_kfids,
     )
     return descendant_kfids
 
 
 def find_descendants_by_filter(
-    host, endpoint, filter, include_gfs_with_external_contributors=False
+    host, endpoint, filter, ignore_gfs_with_hidden_external_contribs
 ):
     """
     Like find_descendants_by_kfids but starts with an endpoint filter instead
@@ -142,6 +173,34 @@ def find_descendants_by_filter(
     """
     endpoint_kfids = set(yield_kfids(host, endpoint, filter, show_progress=True))
     kfid_sets = find_descendants_by_kfids(
-        host, endpoint, endpoint_kfids, include_gfs_with_external_contributors
+        host, endpoint, endpoint_kfids, ignore_gfs_with_hidden_external_contribs
     )
     return kfid_sets
+
+
+def hide_all_descendants_by_filter(host, endpoint, filter):
+    """
+    Be aware that this and show_all_descendants_by_filter are not symmetrical.
+
+    Hiding hides partially contributed descendants, but showing only shows
+    partially contributed descendants if all other contributors are visible.
+    If you anticipate needing symmetrical behavior, keep a record of what you
+    change.
+    """
+    desc = find_descendants_by_filter(host, endpoint, filter, False)
+    for k, v in desc.items():
+        hide_kfids(host, v)
+
+
+def show_all_descendants_by_filter(host, endpoint, filter):
+    """
+    Be aware that this and hide_all_descendants_by_filter are not symmetrical.
+
+    Hiding hides partially contributed descendants, but showing only shows
+    partially contributed descendants if all other contributors are visible.
+    If you anticipate needing symmetrical behavior, keep a record of what you
+    change.
+    """
+    desc = find_descendants_by_filter(host, endpoint, filter, True)
+    for k, v in desc.items():
+        show_kfids(host, v)
