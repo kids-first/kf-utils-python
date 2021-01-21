adfa8f8bd34c01a82fc1131a76bbda8d6e13d26f
Avi Kelman <patcherton.fixesthings@gmail.com>
2021-01-21 09:56:41 -0700

    :bug: Fix find_packages invocation
    closes #19

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kids-first/kf-python-utils",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     python_requires=">=3.6, <4",
     install_requires=requirements,
 )
